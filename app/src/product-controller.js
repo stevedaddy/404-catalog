46f4595dfeb3b18e99957e54660ece441674a9e6
Steve T <stevethewebguy@gmail.com>
2016-05-26 07:54:45 -0700

    Styles for catalog home

--- app/src/product-controller.js
+++ app/src/product-controller.js
@@ -2,5 +2,6 @@
     angular.module('catalogApp')
         .controller('ProductCtrl', function($scope, $state, $stateParams, dataService){
                 $scope.product = dataService.productByProductId($stateParams.productId);
+            console.log($scope.product);
         });
 }());
