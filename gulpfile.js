d919d6422a7c40c5a4bfea8d1176fc1a4901e0ea
Steve T <stevethewebguy@gmail.com>
2016-05-31 01:23:30 -0700

    fixes for build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,69 +1,45 @@
-//
-//var gulp = require('gulp'),
-//    connect = require('gulp-connect'),
-//    Proxy = require('gulp-connect-proxy');
-//
-//gulp.task('connect', function() {
-//    connect.server({
-//        root: 'app/',
-//        middleware: function (connect, opt) {
-//            opt.route = '/proxy';
-//            var proxy = new Proxy(opt);
-//            return [proxy];
-//        }
-//    });
-//});
-//gulp.task('default', ['connect']);
 
 var Proxy = require('gulp-connect-proxy');
-var gulp = require('gulp'),
-    sass = require('gulp-sass'),
+var gulp = require('gulp');
+var sass = require('gulp-ruby-sass');
     runSequence = require('run-sequence'),
     templateCache = require('gulp-angular-templatecache'),
     postcss = require('gulp-postcss');
 var connect = require('gulp-connect');
 var uglify = require('gulp-uglify');
 var ngmin = require('gulp-ngmin');
-var minifyHtml = require('gulp-minify-html');
-var minifyCss = require('gulp-minify-css');
 var usemin = require('gulp-usemin');
 var rev = require('gulp-rev');
 var clean = require('gulp-clean');
 var ngAnnotate = require('gulp-ng-annotate');
+var server = require('gulp-server-livereload');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+var watchLess = require('gulp-watch-less');
+var less = require('gulp-less');
 
-
-gulp.task('copy-html-files', function() {
-    gulp.src(['./app/**/*.html', './app/resources/catalog.json', '!./app/index.html'], {base: './app'})
-        .pipe(gulp.dest('build/'));
-});
-
-gulp.task('sass', function() {
-    return gulp.src('./app/styles/*.scss')
-        .pipe(sass())
-        .pipe(postcss([require('autoprefixer')]))
-        .pipe(gulp.dest('./css'));
-});
-
-gulp.task('templates', function() {
-    return gulp.src('./app/*.html')
-        .pipe(templateCache('templates.js', {
-            root: 'app/',
-            module: 'catalogApp'
-        }))
-        .pipe(gulp.dest('./src'));
-});
-
-gulp.task('usemin', function() {
-    gulp.src('!./app/index.html', '')
-        .pipe(usemin({
-            css: [minifyCss(), 'concat', rev()],
-            js: [uglify(), rev()]
-        }))
-        .pipe(gulp.dest('build/'));
+gulp.task('watch', function () {
+    watch('./styles/*.less', batch(function (events, done) {
+        gulp.start('default', done);
+    }));
 
 });
 
-gulp.task('build', ['copy-html-files', 'usemin']);
+gulp.task('webserver', ['watch'], function () {
+    gulp.src('./app')
+        .pipe(server({
+            livereload: {
+                enable: true,
+                filter: function (filename, cb) {
+                    cb(!/\.(sa|le)ss$|node_modules/.test(filename));
+                }
+            },
+            file: 'index.html',
+            directoryListing: false,
+            open: true
+        }));
+
+});
 
 gulp.task('connect', function() {
     connect.server({
@@ -74,6 +50,10 @@ gulp.task('connect', function() {
             return [proxy];
         }
     });
+    return gulp.src('styles/app.less')
+        .pipe(watchLess('styles/app.less'))
+        .pipe(less())
+        .pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['connect']);
